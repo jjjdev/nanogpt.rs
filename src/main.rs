3f980332618ec3f56a51cf47d71ce98d4ac4fd06
JJJ <jjjacksondev@gmail.com>
2024-04-11 12:56:15 -0400

    define and layout project structure via comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+// For now, just define the structure of the project
+
+// the libraries to use::
+// 1) rust equivalent to torch (for tensors)
+// 2) rust equiovalent to torch.nn (for neural networks)
+// 3) cuda bindings for rust to use GPU
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("GPT-2 rust style coming soon!!");
+
+    // Again just get the steps in.  We will organize later.
+
+    // 1) Load the dataset
+
+
+    // 2) Create the simple tokenizer
+
+
+    // 3) Encode the dataset
+
+
+    // 4) Test/train validation split
+
+
+    // 5) Build simple BigramModel (to start and learn)
+    // 5b) Multi-head attention (later)
+
+
+    // 6) Optimizer (take gradients, update based on loss)
+
+
+    // 7) Training loop
+
+    println!("LETS GOOOOOOOO!!");
 }
